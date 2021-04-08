f4150d7f468b866aa8893271c4f6112db3953a62
Robert Głowacki <rglowacki@im.umg.edu.pl>
2021-04-08 23:01:20 +0200

    docs: [develop] JSDcocs added.

--- src/app/app/model/get-search-results.ts
+++ src/app/app/model/get-search-results.ts
@@ -1,11 +1,23 @@
 import { GetSingleResult } from './get-single-result';
 
+/**
+ * Model class for search result.
+ */
 export class GetSearchResults {
 
+  /**
+   * Total number of results.
+   */
   total: number;
 
+  /**
+   * Totoal number of pages.
+   */
   totalPages: number;
 
+  /**
+   * List fo single result.
+   */
   results: Array<GetSingleResult>;
 
 }
